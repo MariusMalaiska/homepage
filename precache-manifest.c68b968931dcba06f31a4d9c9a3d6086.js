31d4c614b64112bd272d4ec37d3f777a13c59d02
Marius <mariusmalaiska@gmail.com>
2020-10-27 15:51:17 +0200

    Updates

---
+++ precache-manifest.c68b968931dcba06f31a4d9c9a3d6086.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc655af7d887968aefe48799816cf1d",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "b03b4fbc1382c8635acf",
+    "url": "/homepage/static/css/main.49d7c462.chunk.css"
+  },
+  {
+    "revision": "28ed9f2bc3773a7cddc4",
+    "url": "/homepage/static/js/2.d6d79838.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homepage/static/js/2.d6d79838.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03b4fbc1382c8635acf",
+    "url": "/homepage/static/js/main.fd516853.chunk.js"
+  },
+  {
+    "revision": "836f55bc99e549bdd710",
+    "url": "/homepage/static/js/runtime-main.3ed86e83.js"
+  },
+  {
+    "revision": "d9933c4e2c272f33b74ef18cdf11a7d5",
+    "url": "/homepage/static/media/Loading.d9933c4e.gif"
+  },
+  {
+    "revision": "833a7e305976ee0221846c9e4b9dd87b",
+    "url": "/homepage/static/media/bunny.833a7e30.svg"
+  },
+  {
+    "revision": "c9fd8da2f5bb4a311b87c3416587bc27",
+    "url": "/homepage/static/media/clouds-and-sun.c9fd8da2.svg"
+  },
+  {
+    "revision": "7f716c912ce60153bd4f24212a42d7c9",
+    "url": "/homepage/static/media/clouds.7f716c91.svg"
+  },
+  {
+    "revision": "94f2669d6aa99a8413a3f65953e03dda",
+    "url": "/homepage/static/media/fog.94f2669d.svg"
+  },
+  {
+    "revision": "59d6ef11e4bd1a2a10f52c465e2059f1",
+    "url": "/homepage/static/media/frost.59d6ef11.svg"
+  },
+  {
+    "revision": "13e4088ac0709dc05f1eeb7aabe27683",
+    "url": "/homepage/static/media/rain.13e4088a.svg"
+  },
+  {
+    "revision": "98ed6da2b2df6d63fa190a6a3e1ca69d",
+    "url": "/homepage/static/media/sleet.98ed6da2.svg"
+  },
+  {
+    "revision": "a91906778656fb2c5ee4ecbb19668aee",
+    "url": "/homepage/static/media/sun-hand-drawn-day-symbol.a9190677.svg"
+  },
+  {
+    "revision": "67d1705e86f17281f7d9ac7890b3a278",
+    "url": "/homepage/static/media/video.67d1705e.mp4"
+  }
+]);
